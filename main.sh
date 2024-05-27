928a15fa31c5f73cf9ce63cf5139de72bd8c4b2d
luohuidong <luohuidong01@126.com>
2024-05-27 20:15:39 +0800

    add shell script for setting up development environment

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install curl vim htop
+# Install Rime Input Method
+sudo apt install ibus-rime
+
+# Install development environment
+source ./setup-dev-env.sh
---
+++ setup-dev-env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt install git
+sudo apt install tmux
+
+sudo apt install build-essential
+
+# Install Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash # install nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+
+# Install Deno
+curl -fsSL https://deno.land/install.sh | sh
+
+# Install Bun
+curl -fsSL https://bun.sh/install | bash
+
+# Install Pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
